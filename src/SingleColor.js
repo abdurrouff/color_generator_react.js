e82e541f876f9232e30e01d796a53dfa68f26c8e
Rawman <rouffaderemi@gmail.com>
2022-08-03 23:56:11 +0100

    color generator react.js

---
+++ src/SingleColor.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import rgbToHex from './utils';
+
+const SingleColor = ({ rgb, weight, index }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
+  const hex = rgbToHex(...rgb);
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeOut);
+  });
+  return (
+    <article
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex);
+      }}
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+    >
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hex}</p>
+      {alert && <p className='alert'>coppied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
